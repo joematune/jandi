3acbf789ce74d0f439c2c641aed9234874341bd7
joematune <joematune@gmail.com>
2020-02-20 12:18:07 +0200

    Add exampleStudents to chrome.storage

--- background.js
+++ background.js
@@ -1,8 +1,35 @@
-
 'use strict';
 
+let exampleStudents = {
+    "112112": {
+        id: "112112",
+        name: "Chingching",
+        gender: "boy"
+    },
+    "424242": {
+        id: "424242",
+        name: "jimmy",
+        gender: "boy"
+    },
+    "848484": {
+        id: "848484",
+        name: "johnny",
+        gender: "boy"
+    },
+    "911911": {
+        id: "911911",
+        name: "jamie",
+        gender: "girl"
+    },
+    "2061728": {
+        id: "2061728",
+        name: "Tuantuan",
+        gender: "boy"
+    }
+}
+
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#aaaaaa' }, function () {
+    chrome.storage.sync.set({ color: '#aaaaaa', students: exampleStudents }, function () {
         console.log("The color is grey.");
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
