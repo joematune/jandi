df1a1d372148b42d4df5bfdf7307f5cf7bf02fb0
joematune <joematune@gmail.com>
2020-03-03 11:36:14 +0200

    Replace exampleStudents with empty object

--- background.js
+++ background.js
@@ -1,14 +1,7 @@
 'use strict';
 
-let exampleStudents = {
-    "2061728": {
-        sId: "2061728",
-        sName: "Tuantuan"
-    }
-}
-
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#aaaaaa', students: exampleStudents }, function () {
+    chrome.storage.sync.set({ color: '#aaaaaa', students: {} }, function () {
         console.log("The color is grey.");
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
